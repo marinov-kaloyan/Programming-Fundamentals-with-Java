c82d32e6155386cf0d7e360319d299b5bafc0936
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-27 21:03:20 +0300

    05. Concat Names

---
+++ L02DataTypesAndVariables/L05ConcatNames.java
@@ -0,0 +1,15 @@
+package L02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class L05ConcatNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String secondName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+
+        System.out.println(firstName + delimiter + secondName);
+    }
+}
