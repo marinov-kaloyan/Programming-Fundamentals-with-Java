8a182997badfc1b70645ecf1a6efb9cfe22ebb9f
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-27 21:07:48 +0300

    08. Lower or Upper

---
+++ L02DataTypesAndVariables/L08LowerOrUpper.java
@@ -0,0 +1,17 @@
+package L02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class L08LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char letter = scanner.nextLine().charAt(0);
+
+        if (letter >= 'a' && letter <= 'z') {
+            System.out.println("lower-case");
+        } else if (letter >= 'A' && letter <= 'Z') {
+            System.out.println("upper-case");
+        }
+    }
+}
