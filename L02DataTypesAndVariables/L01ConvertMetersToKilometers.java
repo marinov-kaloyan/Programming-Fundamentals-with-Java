be1e62c96e24111e5c922288c10e4bd8ea245b1f
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-27 20:53:18 +0300

    Initial Commit

---
+++ L02DataTypesAndVariables/L01ConvertMetersToKilometers.java
@@ -0,0 +1,17 @@
+package L02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class L01ConvertMetersToKilometers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int meters = Integer.parseInt(scanner.nextLine());
+
+        double km = meters / 1000.0;
+
+        System.out.printf("%.2f", km);
+    }
+}
+
+
---
+++ L02DataTypesAndVariables/L02PoundsToDollars.java
@@ -0,0 +1,16 @@
+package L02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class L02PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float pound = Float.parseFloat(scanner.nextLine());
+
+        float dollars = pound * 1.36f;
+
+        System.out.printf("%.3f", dollars);
+    }
+}
+
