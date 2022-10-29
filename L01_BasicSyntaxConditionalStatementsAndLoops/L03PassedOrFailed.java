9e365c6b970859ffbd91941b48a9219b0d12d1fe
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-29 23:24:38 +0300

    03. Passed or Failed

---
+++ L01_BasicSyntaxConditionalStatementsAndLoops/L03PassedOrFailed.java
@@ -0,0 +1,17 @@
+package L01_BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class L03PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+    }
+}
