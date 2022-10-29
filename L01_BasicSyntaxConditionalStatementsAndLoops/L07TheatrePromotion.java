0b9ea40225aa48111fd7d9e0738f371939957f28
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-29 23:28:07 +0300

    07. Theatre Promotion

---
+++ L01_BasicSyntaxConditionalStatementsAndLoops/L07TheatrePromotion.java
@@ -0,0 +1,48 @@
+package L01_BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class L07TheatrePromotion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String day = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        boolean isNotValid = false;
+        int price = 0;
+        if (age >= 0 && age <= 18) {
+            if (day.equals("Weekday")) {
+                price = 12;
+            } else if (day.equals("Weekend")) {
+                price = 15;
+            } else if (day.equals("Holiday")) {
+                price = 5;
+            }
+        } else if (age > 18 && age <= 64) {
+            if (day.equals("Weekday")) {
+                price = 18;
+            } else if (day.equals("Weekend")) {
+                price = 20;
+            } else if (day.equals("Holiday")) {
+                price = 12;
+            }
+        } else if (age > 64 && age <= 122) {
+            if (day.equals("Weekday")) {
+                price = 12;
+            } else if (day.equals("Weekend")) {
+                price = 15;
+            } else if (day.equals("Holiday")) {
+                price = 10;
+            }
+        } else {
+            isNotValid = true;
+        }
+
+        if (isNotValid) {
+            System.out.println("Error!");
+        } else {
+            System.out.printf("%d$", price);
+        }
+    }
+}
