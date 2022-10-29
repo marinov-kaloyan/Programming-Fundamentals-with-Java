2686ca2d5670fef36738517708113c45d3705a15
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-29 23:31:29 +0300

    12. Even Number

---
+++ L01_BasicSyntaxConditionalStatementsAndLoops/L12EvenNumber.java
@@ -0,0 +1,17 @@
+package L01_BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class L12EvenNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        while (num % 2 != 0) {
+            System.out.println("Please write an even number.");
+            num = Integer.parseInt(scanner.nextLine());
+        }
+        System.out.printf("The number is: %d", Math.abs(num));
+    }
+}
