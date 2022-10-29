678232d333a180923a05df71260836dfe32cf430
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-29 23:27:25 +0300

    06. Foreign Languages

---
+++ L01_BasicSyntaxConditionalStatementsAndLoops/L06ForeignLanguages.java
@@ -0,0 +1,25 @@
+package L01_BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class L06ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
