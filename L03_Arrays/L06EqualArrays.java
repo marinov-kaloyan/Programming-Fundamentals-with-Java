b99dcfa7a36fb7a792bc81063db3dce8a0d1b80b
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-30 23:30:00 +0200

    06. Equal Arrays

--- L03_Arrays/L06EqualArrays.java
+++ L03_Arrays/L06EqualArrays.java
@@ -8,11 +8,11 @@ public class L06EqualArrays {
         Scanner scanner = new Scanner(System.in);
 
         int[] firstArr = Arrays
-                .stream(scanner.nextLine().split(" "))
+                .stream(scanner.nextLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
         int[] secondArr = Arrays
-                .stream(scanner.nextLine().split(" "))
+                .stream(scanner.nextLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
