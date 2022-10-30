ebdba40b3ceed3dc72679f14fe534e75dc9aa2f4
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-30 23:25:45 +0200

    03. Sum Even Numbers

---
+++ L03_Arrays/L03SumEvenNumbers.java
@@ -0,0 +1,28 @@
+package L03_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L03SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String lineInput = scanner.nextLine();
+
+        int[] numbersArr = Arrays
+                .stream(lineInput.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+        for (int i = 0; i < numbersArr.length; i++) {
+            int currentElement = numbersArr[i];
+
+            if (currentElement % 2 == 0) {
+                sum += currentElement;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
