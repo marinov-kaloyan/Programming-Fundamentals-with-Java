5e968313861260a7fb2eb8ceb977fd4b7c831e25
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-30 23:23:09 +0200

    01. Day of Week

---
+++ L03_Arrays/L01DayOfWeek.java
@@ -0,0 +1,27 @@
+package L03_Arrays;
+
+import java.util.Scanner;
+
+public class L01DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] dayOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+        };
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        if (n >= 1 && n <= 7) {
+            System.out.println(dayOfWeek[n - 1]);
+        } else {
+            System.out.println("Invalid day!");
+        }
+    }
+}
