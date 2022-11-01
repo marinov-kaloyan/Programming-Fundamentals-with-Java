a4f471fa3edfd0a8d5b121c355b82e597e8a41ae
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-01 22:48:14 +0200

    02. Grades

--- L04_Methods/L02Grades.java
+++ L04_Methods/L02Grades.java
@@ -2,7 +2,7 @@ package L04_Methods;
 
 import java.util.Scanner;
 
-public class P02Grades {
+public class L02Grades {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
