514fc7de4c631681112aca12ce461eddb8a12208
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-03 23:49:25 +0200

    06. List of Products

---
+++ L05_Lists/L06_ListOfProducts.java
@@ -0,0 +1,27 @@
+package L05_Lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class L06_ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<String> productList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String product = scanner.nextLine();
+
+            productList.add(product);
+        }
+
+        Collections.sort(productList);
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d.%s%n", i + 1, productList.get(i));
+        }
+    }
+}
