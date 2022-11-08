2811965b2c60133532722795c819f035283ea973
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-08 23:46:44 +0200

    09. Palindrome Integers

---
+++ E04_Methods/E09_PalindromeIntegers.java
@@ -0,0 +1,27 @@
+package E04_Methods;
+
+import java.util.Scanner;
+
+public class E09_PalindromeIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+
+        while (!number.equals("END")) {
+            if (!isPalindrom(number)) {
+                System.out.println("false");
+            } else {
+                System.out.println("true");
+            }
+            number = scanner.nextLine();
+        }
+    }
+
+    private static boolean isPalindrom(String number) {
+        String reversedString = "";
+        for (int i = number.length() - 1; i >= 0; i--) {
+            reversedString += number.charAt(i);
+        }
+        return number.equals(reversedString);
+    }
+}
