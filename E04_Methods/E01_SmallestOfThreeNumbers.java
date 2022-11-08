241e4f6e5c3825bfc313c1fdef0f41cf091b1698
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-08 23:35:26 +0200

    01. Smallest of Three Numbers

---
+++ E04_Methods/E01_SmallestOfThreeNumbers.java
@@ -0,0 +1,30 @@
+package E04_Methods;
+
+import java.util.Scanner;
+
+public class E01_SmallestOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+        int number3 = Integer.parseInt(scanner.nextLine());
+
+
+        printSmallestNumber(number1, number2, number3);
+    }
+
+    private static void printSmallestNumber (int n1, int n2, int n3) {
+        if (n1 <= n2 && n1 <= n3) {
+            System.out.println(n1);
+        }
+
+        else if (n2 <= n1 && n2 <= n3) {
+            System.out.println(n2);
+        }
+
+        else if (n3 <= n1 && n3 <= n2) {
+            System.out.println(n3);
+        }
+    }
+
+}
