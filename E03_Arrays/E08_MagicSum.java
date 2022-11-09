8a622c768db840c45a2e799483af7fd3b30b70d4
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-09 23:29:31 +0200

    08. Magic Sum

---
+++ E03_Arrays/E08_MagicSum.java
@@ -0,0 +1,25 @@
+package E03_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class E08_MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e)).toArray();
+        int sum = Integer.parseInt(scanner.nextLine());
+        for (int index = 0; index <= numbers.length - 1; index++) {
+
+            int number = numbers[index];
+            for (int i = index + 1; i <= numbers.length - 1; i++) {
+
+                int number2 = numbers[i];
+                if (number + number2 == sum) {
+                    System.out.printf("%d %d%n", number, number2);
+                }
+            }
+        }
+
+    }
+}
