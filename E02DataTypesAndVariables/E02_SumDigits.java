1a10b36f8c852646bd3c3ea3cc0373507a7107dc
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-10 11:14:45 +0200

    02. Sum Digits

---
+++ E02DataTypesAndVariables/E02_SumDigits.java
@@ -0,0 +1,17 @@
+package E02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class E02_SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        while (number > 0) {
+            int lastDigit = number % 10;
+            sum += lastDigit;
+            number = number / 10;
+        }
+        System.out.println(sum);
+    }
+}
