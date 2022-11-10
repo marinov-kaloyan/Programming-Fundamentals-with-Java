778f998e88da8fd3cb077ccd03278ad5bca1a150
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-10 11:20:12 +0200

    05. Print Part Of ASCII Table

---
+++ E02DataTypesAndVariables/E05_PrintPartOfAscii.java
@@ -0,0 +1,15 @@
+package E02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class E05_PrintPartOfAscii {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int startCode = Integer.parseInt(scanner.nextLine());
+        int endCode = Integer.parseInt(scanner.nextLine());
+        for (int code = startCode; code <= endCode; code++) {
+            char symbol = (char) code;
+            System.out.print(symbol + " ");
+        }
+    }
+}
