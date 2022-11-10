470ccc504864ece1799735cfb9b483d84d6ee963
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-10 11:07:33 +0200

    09. Orders

---
+++ E01_BasicSyntaxConditionalStatementsAndLoops/E09_Orders.java
@@ -0,0 +1,27 @@
+package E01_BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class E09_Orders {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int countOfOrders = Integer.parseInt(scanner.nextLine());
+        double coffeePrice = 0;
+        double totalPrice = 0;
+
+        for (int i = 0; i < countOfOrders; i++) {
+            double pricePerCapsule = Double.parseDouble(scanner.nextLine());
+            int daysInMonth = Integer.parseInt(scanner.nextLine());
+            int capsulesCount = Integer.parseInt(scanner.nextLine());
+
+            coffeePrice = ((daysInMonth * capsulesCount) * pricePerCapsule);
+            totalPrice += coffeePrice;
+
+            System.out.printf("The price for the coffee is: $%.2f%n", coffeePrice);
+        }
+        System.out.printf("Total: $%.2f%n", totalPrice);
+    }
+}
