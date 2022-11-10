1ea9dd74e32e1f486493d95dbfbd30589da0b877
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-10 11:26:46 +0200

    10. Poke Mon

---
+++ E02DataTypesAndVariables/E10_PokeMon.java
@@ -0,0 +1,25 @@
+package E02DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class E10_PokeMon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int power = Integer.parseInt(scanner.nextLine());
+        int distance = Integer.parseInt(scanner.nextLine());
+        int exhaustionFactor = Integer.parseInt(scanner.nextLine());
+        int count = 0;
+        int startPower = power;
+        while (power >= distance) {
+            power -= distance;
+            count++;
+            if (power == startPower * 0.5) {
+                if (exhaustionFactor > 0) {
+                    power = power / exhaustionFactor;
+                }
+            }
+        }
+        System.out.println(power);
+        System.out.println(count);
+    }
+}
