c61d8c8cef64436270f8e6ffdafee1bdebc9198e
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-14 23:02:46 +0200

    01. Furniture

---
+++ E09_RegularExpressions/E01_Furniture.java
@@ -0,0 +1,39 @@
+package E09_RegularExpressions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class E01_Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> furniture = new ArrayList<>();
+        double totalSum = 0;
+
+         String regex = ">>(?<furnitureName>\\w+)<<(?<price>\\d+.?\\d*)!(?<quantity>\\d+)";
+        Pattern pattern = Pattern.compile(regex);
+
+        String input = scanner.nextLine();
+        while (!input.equals("Purchase")) {
+             Matcher matcher = pattern.matcher(input);
+              if (matcher.find()) {
+                String furnitureName = matcher.group("furnitureName");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+
+                furniture.add(furnitureName);
+                double currentFurniturePrice = price * quantity;
+                totalSum += currentFurniturePrice;
+
+            }
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Bought furniture:");
+        furniture.forEach(System.out::println);
+        System.out.printf("Total money spend: %.2f", totalSum);
+    }
+}
