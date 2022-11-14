5ec9fb7302c9a1e91c13e7909a53fcbb468f886a
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-14 22:58:25 +0200

    08. Letters Change Numbers

---
+++ E08_TextProcessing/E08_LettersChangeNumbers.java
@@ -0,0 +1,43 @@
+package E08_TextProcessing;
+
+import java.util.Scanner;
+
+public class E08_LettersChangeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] codes = input.split("\\s+");
+
+        double totalSum = 0;
+        for (String code : codes) {
+            double number = getModifiedNumber(code);
+            totalSum += number;
+        }
+
+        System.out.printf("%.2f", totalSum);
+    }
+
+    private static double getModifiedNumber(String code) {
+        char firstLetter = code.charAt(0);
+        char secondLetter = code.charAt(code.length() - 1);
+
+        double number = Double.parseDouble(code.replace(firstLetter, ' ').replace(secondLetter, ' ').trim());
+
+
+        if (Character.isUpperCase(firstLetter)) {
+            int positionUpperLetter = (int) firstLetter - 64;
+            number /= positionUpperLetter;
+        } else {
+            int positionLowerCase = (int) firstLetter - 96;
+            number *= positionLowerCase;
+        }
+        if (Character.isUpperCase(secondLetter)) {
+            int positionUpperLetter = (int) secondLetter - 64;
+            number -= positionUpperLetter;
+        } else {
+            int positionLowerCase = (int) secondLetter - 96;
+            number += positionLowerCase;
+        }
+        return number;
+    }
+}
